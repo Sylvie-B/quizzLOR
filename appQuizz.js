f0d6098fcda8876e24f272c55610294e24826a4f
Sylvie-B <unsolo@hotmail.fr>
2021-01-23 18:34:12 +0100

    next question function

--- appQuizz.js
+++ appQuizz.js
@@ -1,122 +1,72 @@
 let quest = [
     {
-        "text" : "Qui est Frodon ?",
+        "txt" : "Qui est Frodon ?",
         "good" : 0,
-        "choice" : [
-            "Un hobbit de la Comtée",
-            "Le jardinier de Bilbon",
-            "Un magicien",
-            "Un futur pilote de X-Wing"
-        ],
+        "choice" : ["Un hobbit de la Comtée", "Le jardinier de Bilbon", "Un magicien", "Un futur pilote de X-Wing"],
         "answer" : "Frodon est un hobbit de la Comtée.",
         "pict" : "url('')",
     },
     {
-        "text" : "Quel est son lien de parenté avec Bilbon ?",
+        "txt" : "Quel est son lien de parenté avec Bilbon ?",
         "good" : 3,
-        "choice" : [
-            "Frère",
-            "Aucun lien",
-            "Fils",
-            "Neveu",
-        ],
+        "choice" : ["Frère", "Aucun lien", "Fils", "Neveu"],
         "answer" : "Il est son neveu adoptif.",
         "pict" : "url('')",
     },
     {
-        "text" : "Combien de personne forme la communauté de l'anneau ?",
+        "txt" : "Combien de personne forme la communauté de l'anneau ?",
         "good" : 2,
-        "choice" : [
-            "3",
-            "6",
-            "9",
-            "12",
-        ],
-        "answer" : "A leur départ, la communauté est formée de : 1 magicien, " +
+        "choice" : ["3", "6", "9", "12"],
+        "answer" : "A son départ, la communauté est formée de : 1 magicien, " +
             "2 hommes, 1 nain, 1 elfe et 4 hobbits, donc 9 en tout",
         "pict" : "url('')",
     },
     {
-        "text" : "Gimli est un ",
+        "txt" : "Gimli est un ",
         "good" : 1,
-        "choice" : [
-            "elfe",
-            "nain",
-            "homme",
-            "hobbit",
-        ],
+        "choice" : ["elfe", "nain", "homme", "hobbit"],
         "answer" : "Gimli est le représentant des nains au sein de la communauté.",
         "pict" : "url('')",
     },
     {
-        "text" : "Quel est le surnom d'Aragorn ?",
+        "txt" : "Quel est le surnom d'Aragorn ?",
         "good" : 0,
-        "choice" : [
-            "Grand-pas",
-            "Long-bow",
-            "Longue-barbe",
-            "Roitelet",
-        ],
-        "answer" : "Aragorn pour surnom Grand-pas, mais il en a d'autre...",
+        "choice" : ["Grand-pas", "Long-bow", "Longue-barbe", "Roitelet"],
+        "answer" : "Aragorn a pour surnom Grand-pas, mais il en a d'autre...",
         "pict" : "url('')",
     },
     {
-        "text" : "Rendez-vous est donné avec Gandalf à l'auberge :",
+        "txt" : "Rendez-vous est donné avec Gandalf à l'auberge :",
         "good" : 1,
-        "choice" : [
-            "du Dragon vert",
-            "du Poney fringant",
-            "du Chaudron baveu",
-            "de la Cantina",
-        ],
+        "choice" : ["du Dragon vert", "du Poney fringant", "du Chaudron baveu", "de la Cantina"],
         "answer" : "Ils se donnent rendez-vous à l'auberge du Poney fringant",
         "pict" : "url('')",
     },
     {
-        "text" : "Combien de nazgul se lancent à leur poursuite",
+        "txt" : "Combien de nazgul se lancent à leur poursuite",
         "good" : 3,
-        "choice" : [
-            "1",
-            "3",
-            "6",
-            "9",
-        ],
+        "choice" : ["1", "3", "6", "9"],
         "answer" : "Les spectres sont au nombre de 9.",
         "pict" : "url('')",
     },
     {
-        "text" : "Trouvé l'intrus",
+        "txt" : "Trouvé l'intrus",
         "good" : 2,
-        "choice" : [
-            "Gandalf le Blanc",
-            "Gandalf le Gris",
-            "Gandalf le Brun",
-            "Mithrandir",
-        ],
+        "choice" : ["Gandalf le Blanc", "Gandalf le Gris", "Gandalf le Brun", "Mithrandir"],
         "answer" : "\"Le Brun\" ne fait pas partie des surnoms de Gandalf, mais il en a d'autres...",
         "pict" : "url('')",
     },
     {
-        "text" : "Qu'est-ce que le Lembas ?",
+        "txt" : "Qu'est-ce que le Lembas ?",
         "good" : 1,
-        "choice" : [
-            "la langue des elfe",
-            "du pain elfique",
-            "le minerai de la Moria",
-            "l'herbe à pipe",
-        ],
+        "choice" : ["la langue des elfe", "du pain elfique", "le minerai de la Moria", "l'herbe à pipe"],
         "answer" : "Le pain elfique, une bouchée suffit à nourrir un adulte pour la journée.",
         "pict" : "url('')",
     },
     {
-        "text" : "Avec quelle créature combat Gandalf sur le pont la mine ?",
+        "txt" : "Avec quelle créature combat Gandalf sur le pont la mine ?",
         "good" : 3,
-        "choice" : [
-            "un dragon",
-            "un orque",
-            "un balrog",
-            "Sauron",
-        ],
+        "choice" : ["un dragon", "un orque", "un balrog", "Sauron"],
         "answer" : "Il combat le balrog et lui dit : :\"Vous ne passerz-pas !\"",
         "pict" : "url('')",
     }
@@ -124,48 +74,63 @@ let quest = [
 
 $('#container').css('background-color', "#ff7f5080");
 // set display
-$('#nbr').text(quest.length);
-$('#answer').hide();
-// beginning screen to screen question 1
+$('.screen2').hide();
+$('#nbr').text(quest.length); // number of quest
 
+// start
 $('#start').click(function (){
-    // hidden start button
-    $('#start').hide();
-    // display first quest (index 0)
-    $('#questNbr').show().text('Question ' + 1);
-    $('#quest').text(quest[0].text)
-    $('#answer').show();
-    // one choice for each case
+    $('.screen1').hide();
+    //=> function nextQuest param idx = 0;
+    $('.screen2').slideDown(800);
+    nextQuest(0);
+});
+
+let idx = 0;
+let score = 0;
+
+// case
+// idx = 1;
+//  si idx < 10 {
+//  nextQuest(idx);
+//
+//
+// $('.case').click(function (){
+// //     get quest idx ?
+// let a = $('#quest').text();
+// console.log(a);
+
+//     // display quest index goodAns.length
+//
+//     $('#quest').text(quest[a].text)
+//     $('#answer').show();
+//     // one choice for each case
+//     $('.case').each(function (i){
+//         $(this).text(quest[a].choice[i]);
+//     })
+//
+//     console.log(quest[a-1].good);
+//     let idx = $(this).index();
+//     console.log(idx);
+//
+//     if($(this).index() === quest[a-1].good){
+//         let ref = goodAns.push('Bonne réponse');
+//     }
+//     else {
+//         let ref = goodAns.push(quest[a].answer);
+//     }
+//
+//
+//     console.log(goodAns);
+//     console.log(goodAns.length);
+// })
+
+// next question
+function nextQuest(idx){
+    $('#questNbr').show().text('Question ' + (idx + 1));
+    $('#quest').text(quest[idx].txt);
+
+// one choice for each case
     $('.case').each(function (i){
         $(this).text(quest[0].choice[i]);
     })
-})
-
-let goodAns = [];
-
-$('.case').click(function (){
-    let a = goodAns.length + 1;
-    // display quest index goodAns.length
-    $('#questNbr').show().text('Question ' + (a + 1));
-    $('#quest').text(quest[a].text)
-    $('#answer').show();
-    // one choice for each case
-    $('.case').each(function (i){
-        $(this).text(quest[a].choice[i]);
-    })
-
-    console.log(quest[a-1].good);
-    let idx = $(this).index();
-    console.log(idx);
-
-    if($(this).index() === quest[a-1].good){
-        let ref = goodAns.push('Bonne réponse');
-    }
-    else {
-        let ref = goodAns.push(quest[a].answer);
-    }
-
-
-    console.log(goodAns);
-    console.log(goodAns.length);
-})
+}
