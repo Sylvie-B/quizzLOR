1aec7b658ff6c586d4199d5b8cf0771a66dfcf5d
Sylvie-B <unsolo@hotmail.fr>
2021-01-28 23:15:30 +0100

    check img & responsive

--- appQuizz.js
+++ appQuizz.js
@@ -4,77 +4,66 @@ let quest = [
         "good" : 0,
         "choice" : ["Un hobbit de la Comtée", "Le jardinier de Bilbon", "Un magicien", "Un futur pilote de X-Wing"],
         "answer" : "Frodon est un hobbit de la Comtée.",
-        "pict" : "./imgLord/frodo.jpg",
     },
     {
         "txt" : "Quel est son lien de parenté avec Bilbon ?",
         "good" : 3,
         "choice" : ["Frère", "Aucun lien", "Fils", "Neveu"],
         "answer" : "Frodon est le neveu de Bilbon.",
-        "pict" : "./imgLord/bilbon.jpg",
     },
     {
         "txt" : "Lors du conseil d'Elrond, combien de personne forment la communauté de l'anneau ?",
         "good" : 2,
         "choice" : ["3", "6", "9", "12"],
         "answer" : "Ils sont 9 dans la communauté : 1 magicien, 2 hommes, 1 nain, 1 elfe et 4 hobbits.",
-        "pict" : "./imgLord/elrond.jpg",
     },
     {
         "txt" : "Gimli est un ",
         "good" : 1,
         "choice" : ["elfe", "nain", "homme", "hobbit"],
         "answer" : "Gimli est le représentant des nains au sein de la communauté.",
-        "pict" : "./imgLord/communaute.jpg",
     },
     {
         "txt" : "Quel est le surnom d'Aragorn ?",
         "good" : 0,
         "choice" : ["Grand-pas", "Long-bow", "Longue-barbe", "Roitelet"],
         "answer" : "Aragorn a pour surnom Grand-pas.",
-        "pict" : "./imgLord/inTheShadow.jpg",
     },
     {
         "txt" : "Rendez-vous est donné avec Gandalf à l'auberge :",
         "good" : 1,
         "choice" : ["du Dragon vert", "du Poney fringant", "du Chaudron baveu", "de la Cantina"],
         "answer" : "Ils se donnent rendez-vous à l'auberge du Poney fringant.",
-        "pict" : "./imgLord/auberge.jpg",
     },
     {
         "txt" : "Combien de nazgul se lancent à leur poursuite",
         "good" : 3,
         "choice" : ["1", "3", "6", "9"],
         "answer" : "Les spectres sont au nombre de 9.",
-        "pict" : "./imgLord/nazgul.jpg",
     },
     {
         "txt" : "Trouvé l'intrus",
         "good" : 2,
         "choice" : ["Gandalf le Blanc", "Le trouble paix", "Gandalf le Brun", "Mithrandir"],
         "answer" : "\"Le Brun\" ne fait pas partie des surnoms de Gandalf.",
-        "pict" : "./imgLord/moria.jpg",
     },
     {
         "txt" : "Qu'est-ce que le Lembas ?",
         "good" : 1,
         "choice" : ["Un steak de warg", "du pain elfique", "le minerai de la Moria", "l'herbe à pipe"],
         "answer" : "Une bouchée de lembas, le pain elfique, suffit à nourrir un adulte pour la journée.",
-        "pict" : "./imgLord/chaudron.jpg",
     },
     {
         "txt" : "Avec quelle créature combat Gandalf sur le pont la mine ?",
         "good" : 2,
         "choice" : ["un dragon", "un orque", "un balrog", "Sauron"],
         "answer" : "Il combat le balrog.",
-        "pict" : "./imgLord/gandalf.jpg",
     }
 ]
 
 let idx = 0;
 let choice = $('.case');
 let score = 0;
-// todo condition for nbr faulse
 let goodAns = [];
 let wrong = 0;
 
@@ -87,16 +76,16 @@ $('#start').click(function (){
     nextQuest(0);
     $('.screen2').slideDown(500);
 });
-// '<img src=' + quest[x].pict + '/>'
-// first answer
+
+// listener click on choices
 choice.click(function () {
-    if((score + wrong) < 9){
+    if((score + wrong) < 9){        // if answer number index < 9
         check($(this), idx);
         idx++;
         setTimeout(nextQuest, 500, idx);
     }
     else {
-        check($(this), idx);
+        check($(this), idx);        // 10th answer
         $('#answer').hide();
         $('#questNbr').text('votre score est de : ' + score);
         // good answer in screenEnd
@@ -110,7 +99,7 @@ choice.click(function () {
             console.log("<3 : " + wrong);
         }
         else if(wrong < 8){
-            $('#quest').text('Relisez vos slide !');
+            $('#quest').text('Relisez vos slides !');
             $('.side').empty().append('<img alt="logo" src="imgLord/ring.png"/>');
             console.log("<8 : " + wrong);
             $('#screenEnd').slideDown(500);
@@ -125,7 +114,7 @@ choice.click(function () {
     }
 })
 
-// restart
+
 $('#restart').click(function (){
     firstScreen();
     $('#goodAns').empty();
@@ -161,7 +150,6 @@ function check(userChoice,idx){
 
 // function display the question index=idx
 function nextQuest(idx){
-    $('.side').empty().append('<img alt="logo" src='+ quest[idx].pict +'/>');
     $('#questNbr').text('Question ' + (idx + 1));
     $('#quest').text(quest[idx].txt);
 // one choice for each case
